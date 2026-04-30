d6f9ceed83144e59d6fb98d9363ddc0806378814
YarAllex <32192351+YarAllex@users.noreply.github.com>
2026-04-30 18:31:10 +0300

    build: switch publishing to Gradle Plugin Portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21" apply false
+    id("com.gradle.plugin-publish") version "1.3.0" apply false
     id("com.diffplug.spotless") version "6.25.0" apply false
     id("org.jetbrains.kotlinx.kover") version "0.9.1" apply false
 }
